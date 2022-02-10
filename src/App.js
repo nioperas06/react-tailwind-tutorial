a78cdff04888b15b751b3cf9b14da58e556e065a
Junior Gantin <nioperas06@gmail.com>
2022-02-10 14:35:04 +0100

    wip

--- src/App.js
+++ src/App.js
@@ -1,48 +1,56 @@
 import './App.css';
+import React from 'react';
 import Header from './Header';
 import Product from './Product';
-import React, { useEffect, useState } from 'react';
 
 
-const App = () => {
+function App() {
+    const [loading, setLoading] = React.useState(true);
+    const [products, setProducts] = React.useState([]);
 
-    let products = []
+    React.useEffect(() => {
+        fetch('https://fakestoreapi.com/products')
+            .then(results => results.json())
+            .then(data => {
+                setProducts(data)
+            })
+            .catch((error) => console.log(error));
+    }, []);
 
-    useEffect(() => {
-        const url = 'https://fakestoreapi.com/products'
-
-        const fetchData = async() => {
-            try {
-                const response = await fetch(url)
-                const json = await response.json()
-
-                products = json
-            } catch (error) {
-                console.error(error)
-            }
+    React.useEffect(() => {
+        if (products.length !== 0) {
+            setLoading(false);
         }
-
-        fetchData()
-
-    }, [])
+    }, [products]);
 
     return (
         <div>
-                <Header />
-
-                <div className="flex flex-wrap -mx-4 overflow-hidden p-5">
-                    <div className="my-4 px-4 w-1/3 overflow-hidden" >
-                        <Product name="Fjallraven - Foldsack No. 1 Backpack" price="10" image="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" />
-                    </div>
-                    <div className="my-4 px-4 w-1/3 overflow-hidden" >
-                        <Product name="Mens Casual Premium Slim Fit T-Shirts" price="20" image="https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg" />
-                    </div>
-                    <div className="my-4 px-4 w-1/3 overflow-hidden">
-                        <Product name="Mens Cotton Jacket" price="30" image="https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg" />
+            <Header />
+            <div className="bg-white">
+                <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+                    <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+                        {
+                            loading ?
+                            (
+                                <h1>Loading...</h1>
+                            ) :
+                            (
+                                products.map((product) => (
+                                    <div key={product.id}>
+                                        <Product
+                                            name={product.title}
+                                            price={product.price}
+                                            image={product.image} />
+                                    </div>
+                                ))
+                            )
+                        }
                     </div>
                 </div>
+            </div>
         </div>
     );
 }
+
 
 export default App;
--- src/Header.js
+++ src/Header.js
@@ -2,39 +2,31 @@ function Header() {
     return (
         <nav className="bg-white shadow">
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-            <div className="flex justify-between h-16">
-                <div className="flex">
-                <div className="flex-shrink-0 flex items-center">
-                    <img className="block lg:hidden h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg" alt="Workflow" />
-                    <img className="hidden lg:block h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-logo-indigo-600-mark-gray-800-text.svg" alt="Workflow" />
-                </div>
-                <div className="hidden sm:ml-6 sm:flex sm:space-x-8">
-                    <a href="#" className="border-indigo-500 text-gray-900 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"> Dashboard </a>
-                    <a href="#" className="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"> Team </a>
-                    <a href="#" className="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"> Projects </a>
-                    <a href="#" className="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"> Calendar </a>
-                </div>
-                </div>
-                <div className="hidden sm:ml-6 sm:flex sm:items-center">
-                <button type="button" className="bg-white p-1 rounded-full text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                    <span className="sr-only">View notifications</span>
-                </button>
+                <div className="flex justify-between h-16">
+                    <div className="flex">
+                        <div className="flex-shrink-0 flex items-center">
+                            <a href="#" className="text-2xl font-extrabold text-gray-900">Shop</a>
+                        </div>
+                        <div className="hidden sm:ml-6 sm:flex sm:space-x-8">
+                            <a href="#" className="border-indigo-500 text-gray-900 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"> Home </a>
+                        </div>
+                    </div>
+                    <div className="hidden sm:ml-6 sm:flex sm:items-center">
+                        <button type="button" className="bg-white p-1 rounded-full text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                            <span className="sr-only">View notifications</span>
+                        </button>
 
-                <div className="ml-3 relative">
-                    <div>
-                    <button type="button" className="bg-white rounded-full flex text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
-                        <span className="sr-only">Open user menu</span>
-                        <img className="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
-                    </button>
+                        <div className="ml-3 relative">
+                            <div>
+                                <button type="button" className="bg-white rounded-full flex text-sm focus:outline-none" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 rounded-full" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
+                                    </svg>
+                                </button>
+                            </div>
+                        </div>
                     </div>
                 </div>
-                </div>
-                <div className="-mr-2 flex items-center sm:hidden">
-                <button type="button" className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500" aria-controls="mobile-menu" aria-expanded="false">
-                    <span className="sr-only">Open main menu</span>
-                </button>
-                </div>
-            </div>
             </div>
         </nav>
     )
